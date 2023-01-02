1c43030b7936c86ae357541ef10cf3ef82a78711
AdrianAEI <adria@DESKTOP-7OEMRTE>
2023-01-02 13:04:18 +0100

    test

--- MyBitmap.cs
+++ MyBitmap.cs
@@ -12,11 +12,11 @@ namespace ProjektASM
     internal static class MyBitmap
     {
         public static Bitmap ColorBalance(this Bitmap sourceBitmap, byte blueLevel,
-                                    byte greenLevel, byte redLevel,int numberOfThreads)
+                                     byte greenLevel, byte redLevel, int numberOfThreads)
         {
             BitmapData sourceData = sourceBitmap.LockBits(new Rectangle(0, 0,
-                                        sourceBitmap.Width, sourceBitmap.Height),
-                                        ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+                                            sourceBitmap.Width, sourceBitmap.Height),
+                                            ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
 
 
             byte[] pixelBuffer = new byte[sourceData.Stride * sourceData.Height];
@@ -28,42 +28,40 @@ namespace ProjektASM
             sourceBitmap.UnlockBits(sourceData);
 
 
-            float blue = 0;
-            float green = 0;
-            float red = 0;
-
-
             float blueLevelFloat = 255.0f - blueLevel;
             float greenLevelFloat = 255.0f - greenLevel;
             float redLevelFloat = 255.0f - redLevel;
 
-            Parallel.For(0, pixelBuffer.Length / 4, new ParallelOptions { MaxDegreeOfParallelism = numberOfThreads }, k =>
+            Parallel.For(0, sourceBitmap.Height, row =>
             {
-                int pixelIndex = k * 4;
-                blue = (float)Math.Round(255.0f / blueLevelFloat * (float)pixelBuffer[pixelIndex]);
-                green = (float)Math.Round(255.0f / greenLevelFloat * (float)pixelBuffer[pixelIndex + 1]);
-                red = (float)Math.Round(255.0f / redLevelFloat * (float)pixelBuffer[pixelIndex + 2]);
+                int rowStartIndex = row * sourceData.Stride;
+                for (int k = rowStartIndex; k + 4 < rowStartIndex + sourceData.Stride; k += 4)
+                {
+                    float blue = 255.0f / blueLevelFloat * (float)pixelBuffer[k];
+                    float green = 255.0f / greenLevelFloat * (float)pixelBuffer[k + 1];
+                    float red = 255.0f / redLevelFloat * (float)pixelBuffer[k + 2];
 
-                if (blue > 255) { blue = 255; }
-                else if (blue < 0) { blue = 0; }
+                    if (blue > 255) { blue = 255; }
+                    else if (blue < 0) { blue = 0; }
 
-                if (green > 255) { green = 255; }
-                else if (green < 0) { green = 0; }
+                    if (green > 255) { green = 255; }
+                    else if (green < 0) { green = 0; }
 
-                if (red > 255) { red = 255; }
-                else if (red < 0) { red = 0; }
+                    if (red > 255) { red = 255; }
+                    else if (red < 0) { red = 0; }
 
-                pixelBuffer[pixelIndex] = (byte)blue;
-                pixelBuffer[pixelIndex + 1] = (byte)green;
-                pixelBuffer[pixelIndex + 2] = (byte)red;
+                    pixelBuffer[k] = (byte)blue;
+                    pixelBuffer[k + 1] = (byte)green;
+                    pixelBuffer[k + 2] = (byte)red;
+                }
             });
 
             Bitmap resultBitmap = new Bitmap(sourceBitmap.Width, sourceBitmap.Height);
 
 
             BitmapData resultData = resultBitmap.LockBits(new Rectangle(0, 0,
-                                        resultBitmap.Width, resultBitmap.Height),
-                                       ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+                                            resultBitmap.Width, resultBitmap.Height),
+                                           ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
 
 
             Marshal.Copy(pixelBuffer, 0, resultData.Scan0, pixelBuffer.Length);
@@ -72,5 +70,6 @@ namespace ProjektASM
 
             return resultBitmap;
         }
+
     }
 }
