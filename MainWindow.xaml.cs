bf107c72d8c07906d5fe83602cdd454a06a7a12f
Adrian Piróg <72256525+AdrianAEI@users.noreply.github.com>
2023-01-04 16:52:50 +0100

    added assembly ddl

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,3 +1,120 @@
+<<<<<<< HEAD
+﻿using Microsoft.Win32;
+using ProjektASM;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+using Color = System.Windows.Media.Color;
+
+namespace AssmblerRGB
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool highLevelLanguage=true;
+        private Color colorRGB=new Color();
+        private Bitmap bitmap=new Bitmap(800,600);
+        private uint numberOfThreads = 1;
+        [DllImport(@"C:\Users\adria\source\repos\ProjektASM\x64\Debug\JAAsm.dll")]
+
+        static extern int MyProc1(int a, int b);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private async void ColorSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+           
+            try
+            {
+                Color color = Color.FromRgb((byte)slColorR.Value, (byte)slColorG.Value, (byte)slColorB.Value);
+                this.colorRGB = color;
+            }
+            catch(Exception ) { }
+                
+        }
+        private async void ThreadSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+            try
+            {
+                numberOfThreads = (uint)(int)slValue.Value;     
+            }
+            catch (Exception) { }
+
+        }
+        private void Load_Click(object sender, RoutedEventArgs e)
+        {
+  
+            try
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.ShowDialog();
+                bitmap = (Bitmap)Bitmap.FromFile(openFileDialog.FileName);
+                ImageOne.Source = Convert(bitmap);
+                Button2.IsEnabled = true;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Please provide a photo in the correct format!!!");
+            }
+        }
+        private BitmapImage Convert(Bitmap src)
+        {
+            MemoryStream ms = new MemoryStream();
+            ((System.Drawing.Bitmap)src).Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            ms.Seek(0, SeekOrigin.Begin);
+            image.StreamSource = ms;
+            image.EndInit();
+            return image;
+        }
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            int x = 63, y = 6;
+            int retVal = MyProc1(x, y);
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            ImageTwo.Source = Convert(bitmap.ColorBalanceParallel(colorRGB.B, colorRGB.G, colorRGB.R, numberOfThreads, highLevelLanguage));
+            watch.Stop();
+            timeLabel.Content = "Time: " + watch.Elapsed.Milliseconds.ToString() + " ms";
+            MessageBox.Show("Asm test: " + retVal.ToString() + highLevelLanguage.ToString());
+
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            highLevelLanguage = true;
+        }
+
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+            highLevelLanguage = false;
+        }
+    }
+
+}
+=======
 ﻿using Microsoft.Win32;
 using ProjektASM;
 using System;
@@ -114,3 +231,4 @@ namespace AssmblerRGB
     }
 
 }
+>>>>>>> b550f79463505c97653dc54caf94a10d1a944134
--- MyBitmap.cs
+++ MyBitmap.cs
@@ -1,3 +1,94 @@
+<<<<<<< HEAD
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using System.Windows.Shapes;
+using Rectangle = System.Drawing.Rectangle;
+using System.Security.RightsManagement;
+
+namespace ProjektASM
+{
+    internal static class MyBitmap
+    {
+        public static Bitmap ColorBalanceParallel(this Bitmap sourceBitmap, byte blueLevel,
+                                      byte greenLevel, byte redLevel, uint numberOfThreads,bool highLevelLanguage)
+        {
+            BitmapData sourceData = sourceBitmap.LockBits(new Rectangle(0, 0,
+                                            sourceBitmap.Width, sourceBitmap.Height),
+                                            ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+
+
+            byte[] pixelBuffer = new byte[sourceData.Stride * sourceData.Height];
+
+
+            Marshal.Copy(sourceData.Scan0, pixelBuffer, 0, pixelBuffer.Length);
+
+
+            sourceBitmap.UnlockBits(sourceData);
+
+
+            float blueLevelFloat = blueLevel;
+            float greenLevelFloat = greenLevel;
+            float redLevelFloat = redLevel;
+
+            Parallel.For(0, pixelBuffer.Length, new ParallelOptions { MaxDegreeOfParallelism = (int)numberOfThreads }, k =>
+            {
+                if (k * 4 < pixelBuffer.Length)
+                {
+                    if (highLevelLanguage)
+                    {
+                        int index = k * 4;
+                        float blue = blueLevelFloat / 255.0f * (float)pixelBuffer[index];
+                        float green = greenLevelFloat / 255.0f * (float)pixelBuffer[index + 1];
+                        float red = redLevelFloat / 255.0f * (float)pixelBuffer[index + 2];
+
+                        if (blue > 255) { blue = 255; }
+                        else if (blue < 0) { blue = 0; }
+
+                        if (green > 255) { green = 255; }
+                        else if (green < 0) { green = 0; }
+
+                        if (red > 255) { red = 255; }
+                        else if (red < 0) { red = 0; }
+
+                        pixelBuffer[index] = (byte)blue;
+                        pixelBuffer[index + 1] = (byte)green;
+                        pixelBuffer[index + 2] = (byte)red;
+                    }
+                    else if(highLevelLanguage==false)
+                    {
+
+                    }
+                }
+               
+               
+
+            });
+
+
+            Bitmap resultBitmap = new Bitmap(sourceBitmap.Width, sourceBitmap.Height);
+
+
+            BitmapData resultData = resultBitmap.LockBits(new Rectangle(0, 0,
+                                            resultBitmap.Width, resultBitmap.Height),
+                                           ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+
+
+            Marshal.Copy(pixelBuffer, 0, resultData.Scan0, pixelBuffer.Length);
+            resultBitmap.UnlockBits(resultData);
+
+
+            return resultBitmap;
+        }
+    }
+}
+=======
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing.Imaging;
@@ -78,3 +169,4 @@ namespace ProjektASM
         }
     }
 }
+>>>>>>> b550f79463505c97653dc54caf94a10d1a944134
