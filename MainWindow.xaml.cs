9e902c910bb5b7039e1bfa97fefed49c38b093b5
Adrian Piróg <72256525+AdrianAEI@users.noreply.github.com>
2023-01-05 11:38:07 +0100

    Save button added

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,10 +1,11 @@
-<<<<<<< HEAD
-﻿using Microsoft.Win32;
+
+using Microsoft.Win32;
 using ProjektASM;
 using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Reflection.Emit;
@@ -33,10 +34,12 @@ namespace AssmblerRGB
         private bool highLevelLanguage=true;
         private Color colorRGB=new Color();
         private Bitmap bitmap=new Bitmap(800,600);
+        private Bitmap bitmapToSafe=new Bitmap(800,600);
+        
         private uint numberOfThreads = 1;
-        [DllImport(@"C:\Users\adria\source\repos\ProjektASM\x64\Debug\JAAsm.dll")]
+        //[DllImport(@"C:\Users\adria\source\repos\ProjektASM\x64\Debug\JAAsm.dll")]
 
-        static extern int MyProc1(int a, int b);
+        //static extern int MyProc1(int a, int b);
 
         public MainWindow()
         {
@@ -92,14 +95,30 @@ namespace AssmblerRGB
         }
         private void Start_Click(object sender, RoutedEventArgs e)
         {
-            int x = 63, y = 6;
-            int retVal = MyProc1(x, y);
+            //int x = 63, y = 6;
+            //int retVal = MyProc1(x, y);
+            Button3.IsEnabled = true;
             var watch = System.Diagnostics.Stopwatch.StartNew();
-            ImageTwo.Source = Convert(bitmap.ColorBalanceParallel(colorRGB.B, colorRGB.G, colorRGB.R, numberOfThreads, highLevelLanguage));
+            bitmapToSafe = bitmap.ColorBalanceParallel(colorRGB.B, colorRGB.G, colorRGB.R, numberOfThreads, highLevelLanguage);  
             watch.Stop();
+            ImageTwo.Source = Convert(bitmapToSafe);
             timeLabel.Content = "Time: " + watch.Elapsed.Milliseconds.ToString() + " ms";
-            MessageBox.Show("Asm test: " + retVal.ToString() + highLevelLanguage.ToString());
+            //MessageBox.Show("Asm test: " + retVal.ToString() + highLevelLanguage.ToString());
 
+        }
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {     
+                bitmapToSafe.Save(@"C:\Users\adria\Desktop\savedImages\changedImage2.png", ImageFormat.Png);
+                // bitmap.Save("D:\\Pobrane\\blur3.png");
+                MessageBox.Show("Your photo has been saved!", "Success");
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Incorrect localization");
+            }
+           
         }
 
         private void RadioButton_Checked(object sender, RoutedEventArgs e)
@@ -111,124 +130,11 @@ namespace AssmblerRGB
         {
             highLevelLanguage = false;
         }
-    }
 
-}
-=======
-﻿using Microsoft.Win32;
-using ProjektASM;
-using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using static System.Net.Mime.MediaTypeNames;
-using Color = System.Windows.Media.Color;
-
-namespace AssmblerRGB
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        private Color colorRGB=new Color();
-        private Bitmap bitmap=new Bitmap(800,600);
-        private uint numberOfThreads = 1;
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-        private async void ColorSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-           
-            try
-            {
-                Color color = Color.FromRgb((byte)slColorR.Value, (byte)slColorG.Value, (byte)slColorB.Value);
-                this.colorRGB = color;
-            }
-            catch(Exception ) { }
-                
-        }
-        private async void ThreadSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-
-            try
-            {
-                numberOfThreads = (uint)(int)slValue.Value;     
-            }
-            catch (Exception) { }
-
-        }
-        private void Load_Click(object sender, RoutedEventArgs e)
-        {
-  
-            try
-            {
-                OpenFileDialog openFileDialog = new OpenFileDialog();
-                openFileDialog.ShowDialog();
-                bitmap = (Bitmap)Bitmap.FromFile(openFileDialog.FileName);
-                ImageOne.Source = Convert(bitmap);
-
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("Please provide a photo in the correct format!!!");
-            }
-        }
-        private BitmapImage Convert(Bitmap src)
-        {
-            MemoryStream ms = new MemoryStream();
-            ((System.Drawing.Bitmap)src).Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
-            BitmapImage image = new BitmapImage();
-            image.BeginInit();
-            ms.Seek(0, SeekOrigin.Begin);
-            image.StreamSource = ms;
-            image.EndInit();
-            return image;
-        }
-        private void Start_Click(object sender, RoutedEventArgs e)
-        {
-            var watch = System.Diagnostics.Stopwatch.StartNew();
-            ImageTwo.Source = Convert(bitmap.ColorBalanceParallel(colorRGB.B, colorRGB.G, colorRGB.R, numberOfThreads));
-            watch.Stop();
-            timeLabel.Content = "Time: " + watch.Elapsed.Milliseconds.ToString() + " ms";
-          //  MessageBox.Show(numberOfThreads.ToString());
-
-        }
-
-        private void CheckBox_Checked(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void CheckBox_Checked_1(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void RadioButton_Checked(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
         }
     }
 
 }
->>>>>>> b550f79463505c97653dc54caf94a10d1a944134
--- MyBitmap.cs
+++ MyBitmap.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing.Imaging;
@@ -11,11 +10,15 @@ using System.Collections.Concurrent;
 using System.Windows.Shapes;
 using Rectangle = System.Drawing.Rectangle;
 using System.Security.RightsManagement;
+using System.Windows.Markup;
 
 namespace ProjektASM
 {
     internal static class MyBitmap
     {
+        [DllImport(@"C:\Users\adria\source\repos\ProjektASM\x64\Debug\JAAsm.dll")]
+        extern static int MyProc1(byte[] bitmapBytes,float blue,float green,float red, int k);
+
         public static Bitmap ColorBalanceParallel(this Bitmap sourceBitmap, byte blueLevel,
                                       byte greenLevel, byte redLevel, uint numberOfThreads,bool highLevelLanguage)
         {
@@ -63,7 +66,7 @@ namespace ProjektASM
                     }
                     else if(highLevelLanguage==false)
                     {
-
+                        MyProc1(pixelBuffer, blueLevelFloat, greenLevelFloat, redLevelFloat, k);
                     }
                 }
                
@@ -88,85 +91,3 @@ namespace ProjektASM
         }
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing.Imaging;
-using System.Drawing;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Collections.Concurrent;
-using System.Windows.Shapes;
-using Rectangle = System.Drawing.Rectangle;
-using System.Security.RightsManagement;
-
-namespace ProjektASM
-{
-    internal static class MyBitmap
-    {
-        public static Bitmap ColorBalanceParallel(this Bitmap sourceBitmap, byte blueLevel,
-                                      byte greenLevel, byte redLevel, uint numberOfThreads)
-        {
-            BitmapData sourceData = sourceBitmap.LockBits(new Rectangle(0, 0,
-                                            sourceBitmap.Width, sourceBitmap.Height),
-                                            ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
-
-
-            byte[] pixelBuffer = new byte[sourceData.Stride * sourceData.Height];
-
-
-            Marshal.Copy(sourceData.Scan0, pixelBuffer, 0, pixelBuffer.Length);
-
-
-            sourceBitmap.UnlockBits(sourceData);
-
-
-            float blueLevelFloat = blueLevel;
-            float greenLevelFloat = greenLevel;
-            float redLevelFloat = redLevel;
-
-            Parallel.For(0, pixelBuffer.Length, new ParallelOptions { MaxDegreeOfParallelism = (int)numberOfThreads }, k =>
-            {
-                if (k * 4 < pixelBuffer.Length)
-                {
-                    int index = k * 4;
-                    float blue = blueLevelFloat / 255.0f * (float)pixelBuffer[index];
-                    float green = greenLevelFloat / 255.0f * (float)pixelBuffer[index + 1];
-                    float red = redLevelFloat / 255.0f * (float)pixelBuffer[index + 2];
-
-                    if (blue > 255) { blue = 255; }
-                    else if (blue < 0) { blue = 0; }
-
-                    if (green > 255) { green = 255; }
-                    else if (green < 0) { green = 0; }
-
-                    if (red > 255) { red = 255; }
-                    else if (red < 0) { red = 0; }
-
-                    pixelBuffer[index] = (byte)blue;
-                    pixelBuffer[index + 1] = (byte)green;
-                    pixelBuffer[index + 2] = (byte)red;
-                }
-
-            });
-
-
-            Bitmap resultBitmap = new Bitmap(sourceBitmap.Width, sourceBitmap.Height);
-
-
-            BitmapData resultData = resultBitmap.LockBits(new Rectangle(0, 0,
-                                            resultBitmap.Width, resultBitmap.Height),
-                                           ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
-
-
-            Marshal.Copy(pixelBuffer, 0, resultData.Scan0, pixelBuffer.Length);
-            resultBitmap.UnlockBits(resultData);
-
-
-            return resultBitmap;
-        }
-    }
-}
->>>>>>> b550f79463505c97653dc54caf94a10d1a944134
